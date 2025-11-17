c9ad8039b8715ffaaca62a062dc19e10b1345b4a
Григорий Слоква <Tyuiu.SlokvaGA@outlook.com>
2025-11-17 15:31:56 +0500

    Add project files.

---
+++ Tyuiu.SlokvaGA.Sprint5.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SlokvaGA.Sprint5.Task0.V20.Lib
+{
+    public class DataService : ISprint5Task0V20
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double exp = (2.12*Math.Pow(2,3) + 1.05*Math.Pow(2,2) + 4.1*2*2);
+            exp = Math.Round(exp, 3);
+            File.WriteAllText(path, Convert.ToString(exp));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.SlokvaGA.Sprint5.Task0.V20/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.SlokvaGA.Sprint5.Task0.V20.Lib;
+
+namespace Tyuiu.SlokvaGA.Sprint5.Task0.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Слоква Г. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #5                                                              *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                       *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #20                                                            *");
+            Console.WriteLine("* Выполнил: Слоква Г. А. | ИСПб-25-1                                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дано выражение F(x) = 2.12x^3+1.05x^2+4.1x*2, вычислить его значение   *");
+            Console.WriteLine("* при x = 2, результат сохранить в текстовый файл OutPutFileTask0.txt и  *");
+            Console.WriteLine("* вывести на консоль. Округлить до трёх знаков после запятой.            *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int x = 2;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                               *");
+            Console.WriteLine("************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
