a8604a07ed5abb1c3789cd70fd5db79865d82b53
Григорий Слоква <Tyuiu.SlokvaGA@outlook.com>
2025-11-20 15:06:06 +0500

    Task1

---
+++ Tyuiu.SlokvaGA.Sprint5.Task1.V27.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SlokvaGA.Sprint5.Task1.V27.Lib
+{
+    public class DataService : ISprint5Task1V27
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string tempPath = Path.GetTempPath();
+            string fileName = "OutPutFileTask1.txt";
+            string filePath = Path.Combine(tempPath, fileName);
+            string fileContent = "";
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double fx = 0.0;
+
+                double denominator = Math.Sin(x)-3+x;
+
+                if (denominator == 0)
+                {
+                    fx = 0.0;
+                }
+                else
+                {
+                    fx = -3*x-1.5/denominator + Math.Sin(x)-3+x;
+                }
+
+                fx = Math.Round(fx, 2);
+                fileContent += fx.ToString() + "\n";
+            }
+
+            File.WriteAllText(filePath, fileContent);
+
+            return filePath;
+        }
+    }
+}
---
+++ Tyuiu.SlokvaGA.Sprint5.Task1.V27/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.SlokvaGA.Sprint5.Task1.V27.Lib;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tyuiu.SlokvaGA.Sprint5.Task1.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Слоква Г. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #5                                                              *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #27                                                            *");
+            Console.WriteLine("* Выполнил: Слоква Г. А. | ИСПб-25-1                                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("*                      3x-1,5                                            *");
+            Console.WriteLine("* Дана функция F(x) = ---------- + 2 (произвести табулирование) f(x) на  *");
+            Console.WriteLine("*                     sin(x)-3+x                                         *");
+            Console.WriteLine("* заданном диапазоне [-5;5] с шагом 1. Произвести проверку деления на    *");
+            Console.WriteLine("* ноль. При делении на ноль вернуть значение 0. Результат сохранить в    *");
+            Console.WriteLine("* текстовый файл OutPutFileTask1.txt и вывести на консоль в таблицу.     *");
+            Console.WriteLine("* Значения округлить до двух знаков после запятой.                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                               *");
+            Console.WriteLine("************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
